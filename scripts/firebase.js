d4fb69da2dbf90a1ff16e7e8ada9ba575863635d
mohammed kaif <mkaif555123@gmail.com>
2024-07-28 21:36:55 +0500

    add styling and firebase work

---
+++ scripts/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyA_LBvcDWEiNRorC-LHmmYhQdlTk7PcIAw",
+  authDomain: "full-sign-up-and-sign-in-page.firebaseapp.com",
+  projectId: "full-sign-up-and-sign-in-page",
+  storageBucket: "full-sign-up-and-sign-in-page.appspot.com",
+  messagingSenderId: "1055635935036",
+  appId: "1:1055635935036:web:e9302ae0935d54d85f1839"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
