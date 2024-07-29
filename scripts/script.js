02fb952d64b9eef477df8a1c32b4f72f79f9e59f
mohammed kaif <mkaif555123@gmail.com>
2024-07-29 16:19:54 +0500

    95% work done

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ const typed = new Typed('#element', {
     strings: ['WEB DESIGNER' , 'FRONTEND DEVELPOR' ,'MERN DEVELPOR' ],
     typeSpeed: 50,
 });
+
+// firebase work
+
+import { auth, onAuthStateChanged } from "./firebase.js"
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      window.location.href = "./dashboard.html"
+    } else {
+       window.location.href = "./login.html"
+    }
+ });
